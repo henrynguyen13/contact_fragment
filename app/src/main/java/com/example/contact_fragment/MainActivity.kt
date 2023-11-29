200739972d8ab3c355bd2b68d07a1159467f530c
henrynguyen13 <khoa.nd1301@gmail.com>
2023-11-29 20:40:38 +0700

    first commit

---
+++ app/src/main/java/com/example/contact_fragment/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.contact_fragment
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import com.example.contact_fragment.User
+
+
+class MainActivity : AppCompatActivity(), AddUserFragment.AddUserListener, ListPhoneBookFragment.NavigateToAddUserListener {
+
+    companion object{
+        const val LIST_SIZE = "LIST_SIZE"
+    }
+
+    private lateinit var listPhoneBookFragment: ListPhoneBookFragment
+    private lateinit var addUserFragment: AddUserFragment
+    override fun showOptionsMenu(show: Boolean) {
+        if (show) {
+            supportActionBar?.show()
+        } else {
+            supportActionBar?.hide()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listPhoneBookFragment = ListPhoneBookFragment()
+        listPhoneBookFragment.setNavigateToAddUserListener(this)
+
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, listPhoneBookFragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun addUser(user: User) {
+        listPhoneBookFragment.addUser(user)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+
+    override fun navigateToAddUser() {
+
+        val bundle = Bundle()
+        bundle.putInt(LIST_SIZE, listPhoneBookFragment.getListSize())
+
+        addUserFragment = AddUserFragment()
+        addUserFragment.arguments = bundle
+        addUserFragment.setAddUserListener(this)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, addUserFragment)
+            .addToBackStack("AddUserFragment").commit()
+    }
+
+}
